08c973887983d31858a3a26bf45bbb6a6b2a9a57
ztimc <ztimcz@gmail.com>
2024-05-14 10:12:19 +0800

    1.1.0

--- app/src/main/java/com/sabinetek/vestaesample/viewmoel/AuthViewModel.kt
+++ app/src/main/java/com/sabinetek/vestaesample/viewmoel/AuthViewModel.kt
@@ -2,13 +2,17 @@ package com.sabinetek.vestaesample.viewmoel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sabinetek.vesta.data.api.model.NetworkResult
 import com.sabinetek.vesta.data.api.model.req.Sms
 import com.sabinetek.vesta.data.repository.AuthRepository
 import com.sabinetek.vesta.di.IoDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,15 +21,38 @@ class AuthViewModel @Inject constructor(
     @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
-    fun sendSms() {
-        viewModelScope.launch(dispatcher) {
+    private val _isSendMsg = MutableStateFlow(false)
+    val isSendMsg: MutableStateFlow<Boolean> = _isSendMsg
 
-            Timber.i("fucker !!")
-            val result = repository.senSms(Sms("18501052331"))
-            Timber.e("fucker !! ${result.data?.msg}")
+    private val _intervalSecond = MutableStateFlow(60)
+    val intervalSecond: MutableStateFlow<Int> = _intervalSecond
+
+
+    private var job: Job? = null
+
+    fun sendSms(phone: String) {
+        viewModelScope.launch(dispatcher) {
+            when (val result = repository.senSms(Sms(phone))) {
+                is NetworkResult.Success -> {
+                    _isSendMsg.value = true
+                    viewModelScope.launch(dispatcher) {
+                        repeat(60) {
+                            delay(1000L)
+                            _intervalSecond.value--
+                            if (_intervalSecond.value != 0) {
+                                intervalSecond.value = 60
+                                _isSendMsg.value = false
+                            }
+                        }
+                    }
+                }
+
+                is NetworkResult.Empty -> TODO()
+                is NetworkResult.Error -> TODO()
+                is NetworkResult.Loading -> TODO()
+            }
         }
     }
-
 
 
 }
